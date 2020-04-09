d6d59206d106a6f2f8bc9f74427586b744992a94
huangxinchao <huangxinchao@xbotech.com>
2020-04-09 17:59:30 +0800

    创建项目

---
+++ src/main/java/com/pk/generator/PkGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.pk.generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkGeneratorApplication.class, args);
+	}
+
+}
