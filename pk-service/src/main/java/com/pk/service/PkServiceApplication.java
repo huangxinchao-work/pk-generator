0c5621a823a68529d8006ba75edf0cf1ec6fb5ee
huangxinchao <huangxinchao@xbotech.com>
2020-04-10 09:47:45 +0800

    modle

---
+++ pk-service/src/main/java/com/pk/service/PkServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pk.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PkServiceApplication.class, args);
+    }
+
+}
